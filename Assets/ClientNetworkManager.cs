5e99d2f33465b0ce152ebb189eeaee99eaf121f8
Andrew Erskine <erskine94@gmail.com>
2020-12-10 14:14:44 -0800

    Very simple server

---
+++ Assets/ClientNetworkManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DarkRift;
+using DarkRift.Client;
+using DarkRift.Client.Unity;
+
+public class ClientNetworkManager : MonoBehaviour
+{
+    [SerializeField]
+    UnityClient client;
+
+    public Dictionary<ushort, SimpleNetworkEntity> networkPlayers = new Dictionary<ushort, SimpleNetworkEntity>();
+
+    void Awake() {
+        client.MessageReceived += MessageReceived;
+    }
+
+    void MessageReceived(object sender, MessageReceivedEventArgs e) {
+        using (Message message = e.GetMessage() as Message) {
+            if (message.Tag == Tags.PlayerConnectTag) {
+                PlayerConnect(sender, e);
+            } else if (message.Tag == Tags.PlayerDisconnectTag) {
+                PlayerDisconnect(sender, e);
+            }
+        }
+    }
+
+    void PlayerConnect(object sender, MessageReceivedEventArgs e) {
+        using (Message message = e.GetMessage()) {
+            using (DarkRiftReader reader = message.GetReader()) {
+                ushort ID = reader.ReadUInt16();
+                string playerName = reader.ReadString();
+
+                networkPlayers.Add(ID, new SimpleNetworkEntity(ID, playerName));
+            }
+        }
+    }
+
+    void PlayerDisconnect(object sender, MessageReceivedEventArgs e) {
+        using (Message message = e.GetMessage()) {
+            using (DarkRiftReader reader = message.GetReader()) {
+                networkPlayers.Remove(reader.ReadUInt16());
+            }
+        }
+    }
+}
---
+++ Assets/SimpleNetworkEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleNetworkEntity
+{
+    public ushort networkID;
+    public string playerName;
+
+    public SimpleNetworkEntity(ushort _networkID, string _playerName) {
+        networkID = _networkID;
+        playerName = _playerName;
+    }
+}
