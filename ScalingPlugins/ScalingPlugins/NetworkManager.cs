20f87d8dc454d97e6a9a48524e29fde38fbf05c9
Andrew Erskine <erskine94@gmail.com>
2020-12-20 18:25:33 -0800

    Cleanup for Playfab

---
+++ ScalingPlugins/ScalingPlugins/NetworkManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DarkRift;
+using DarkRift.Server;
+
+namespace ScalingPlugins
+{
+    public class NetworkManager : Plugin
+    {
+        public override bool ThreadSafe => false;
+        public override Version Version => new Version(1, 0, 0);
+        Dictionary<IClient, Player> players = new Dictionary<IClient, Player>();
+
+        public NetworkManager(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+            ClientManager.ClientConnected += ClientConnected;
+            ClientManager.ClientDisconnected += ClientDisconnected;
+        }
+
+        void ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            // When client connects, generate new player data
+            Player newPlayer = new Player(e.Client.ID, RandomString(6, false));
+            players.Add(e.Client, newPlayer);
+
+            // Write player data and tell other connected clients about this player
+            using (DarkRiftWriter newPlayerWriter = DarkRiftWriter.Create())
+            {
+                newPlayerWriter.Write(newPlayer);
+
+                using (Message newPlayerMessage = Message.Create(Tags.PlayerConnectTag, newPlayerWriter))
+                {
+                    foreach (IClient client in ClientManager.GetAllClients().Where(x => x != e.Client))
+                    {
+                        client.SendMessage(newPlayerMessage, SendMode.Reliable);
+                    }
+                }
+            }
+
+            // Tell the client player about all connected players
+            foreach (Player player in players.Values)
+            {
+                Message playerMessage = Message.Create(Tags.PlayerConnectTag, player);
+                e.Client.SendMessage(playerMessage, SendMode.Reliable);
+            }
+
+            e.Client.MessageReceived += OnMessage;
+        }
+
+        void ClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            players.Remove(e.Client);
+
+            using (DarkRiftWriter writer = DarkRiftWriter.Create())
+            {
+                writer.Write(e.Client.ID);
+
+                using (Message message = Message.Create(Tags.PlayerDisconnectTag, writer))
+                {
+                    foreach (IClient client in ClientManager.GetAllClients())
+                    {
+                        client.SendMessage(message, SendMode.Reliable);
+                    }
+                }
+            }
+        }
+
+        string RandomString(int size, bool lowerCase)
+        {
+            Random r = new Random();
+            StringBuilder randString = new StringBuilder(size);
+
+            int start = (lowerCase) ? 97 : 65;
+
+            for (int i = 0; i < size; i++)
+            {
+                randString.Append((char)(26 * r.NextDouble() + start));
+            }
+
+            return randString.ToString();
+        }
+
+        void OnMessage(object sender, MessageReceivedEventArgs e)
+        {
+
+        }
+    }
+}
+
